ddd6bd1e6bad5f5aaa6a3425dac5b081db3a720a
Suha-AlHumaid <Suha.Saleh.elq.js@tuwaiq.edu.sa>
2021-11-17 22:20:22 +0300

    set server

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express"); //to create express app
 const app = express(); //call express
-
+const dotenv = require("dotenv");
+const cors = require("cors");
 app.use(express.json()); //Json data
+dotenv.config();
+const PORT = process.env.PORT;
 
-
-////////////////////////////////////////////////////
-app.listen(PORT, () => {
+///////////////////////////////////////////////////
+app.listen(4000, () => {
   console.log(`server is running ${PORT}`);
 });
