8aa098a2a9940647f92199eaacd7474620078a0c
Suha-AlHumaid <Suha.Saleh.elq.js@tuwaiq.edu.sa>
2021-11-17 22:23:02 +0300

    show all data

--- index.js
+++ index.js
@@ -5,8 +5,19 @@ const cors = require("cors");
 app.use(express.json()); //Json data
 dotenv.config();
 const PORT = process.env.PORT;
+////////////////////////
+let toDos = [
+  { name: "Sleep", isDele: false , id: 0 },//elem.name
+  { name: "Eat", isDele: false , id: 1},//elem
+  { name: "sleep Again", isDele: false, id: 2 },//elem
+];
 
-///////////////////////////////////////////////////
+ ///show todos data
+ app.get("/todos", (req,res)=>{
+  res.status(200).json(toDos)
+})
+
+///////////////////////////
 app.listen(4000, () => {
   console.log(`server is running ${PORT}`);
 });
